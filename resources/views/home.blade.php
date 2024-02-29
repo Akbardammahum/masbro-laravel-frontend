c8cf214a59362fdf4186475dd752550083524558
akbar <akbardammahum02@gmail.com>
2024-02-29 20:58:29 +0700

    refac: add p supre

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,6 +25,8 @@
         <div class="bg-white p-6 w-fit h-fit rounded-lg shadow-lg">
             <h2 class="text-2xl font-bold mb-2 text-gray-800">Card with no image</h2>
             <p class="text-gray-700">This is my cool new card!</p>
+            <p class="text-gray-700">This is my cool new card!</p>
+
         </div>
 
         {{-- card 2 --}}
