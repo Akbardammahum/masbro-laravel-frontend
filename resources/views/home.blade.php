6f1fed5a79d84c019eb93c75036bdcd02c1b1443
akbar <akbardammahum02@gmail.com>
2024-02-29 21:01:26 +0700

    refact: warna

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
 
 </head>
 
-<body class="bg-red-600 w-full mx-auto flex flex-col">
+<body class="bg-blue-600 w-full mx-auto flex flex-col">
     @include('layouts.header')
     <div class="mx-auto w-full h-screen p-20 bg-slate-200 grid grid-cols-2 gap-2">
         {{-- card 1 --}}
@@ -33,6 +33,7 @@
         <div class="bg-white p-6 w-fit h-fit rounded-lg shadow-lg">
             <h2 class="text-2xl font-bold mb-2 text-gray-800">Card with no image</h2>
             <p class="text-gray-700">This is my cool new card!</p>
+
         </div>
     </div>
 </body>
